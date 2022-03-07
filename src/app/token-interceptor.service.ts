45dc471ba00f15f5d5b1c5079644b0bd18859074
gokul <gokulchandan18@gmail.com>
2022-03-07 21:31:31 +0530

    commit

--- src/app/token-interceptor.service.ts
+++ src/app/token-interceptor.service.ts
@@ -5,7 +5,7 @@ import { AuthService } from './auth.service';
 export class TokenInterceptorService implements HttpInterceptor {
 
   constructor(private injector: Injector){}
-  intercept(req, next) {
+  intercept(req: { clone: (arg0: { headers: any; }) => any; headers: { set: (arg0: string, arg1: string) => any; }; }, next: { handle: (arg0: any) => any; }) {
     let authService = this.injector.get(AuthService)
     let tokenizedReq = req.clone(
       {
