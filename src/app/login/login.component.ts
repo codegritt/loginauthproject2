658e0139e4ab68a080538762b978373062c5076d
gokul <gokulchandan18@gmail.com>
2022-03-08 09:31:58 +0530

    commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
-import { Router } from '@angular/router'
+// import { Router } from '@angular/router'
 
 
 @Component({
@@ -13,21 +13,34 @@ export class LoginComponent implements OnInit {
   loginUserData = { email: '',
   password: '',}
 
-  constructor(private _auth: AuthService,
-              private _router: Router) { }
+  // constructor(private _auth: AuthService,
+  //             private _router: Router) { }
+
+
+              constructor(private _auth: AuthService){}
 
   ngOnInit() {
   }
 
   loginUser () {
+    // this._auth.loginUser(this.loginUserData)
+    // .subscribe(
+    //   res => {
+    //     localStorage.setItem('token', res.token)
+    //     this._router.navigate(['/special'])
+    //   },
+    //   err => console.log(err)
+    // ) 
+
     this._auth.loginUser(this.loginUserData)
     .subscribe(
-      res => {
-        localStorage.setItem('token', res.token)
-        this._router.navigate(['/special'])
-      },
-      err => console.log(err)
-    ) 
+     res =>console.log(res),
+     err=>console.log(err)
+    )
+
+ 
+
+
   }
 
 }
