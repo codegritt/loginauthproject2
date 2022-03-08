60083c53874ef565d37e9112e00ee0f224a8b885
gokul <gokulchandan18@gmail.com>
2022-03-08 13:16:33 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { AuthService } from './auth.service';
 export class AppComponent {
   title = 'loginauthproject2';
   
-  constructor(private _authService: AuthService){}
+  constructor(public _authService: AuthService){}
 }
